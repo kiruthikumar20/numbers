42577b5cd201d0916c50ae21b58b8a5fce2eeb2b
Kiruthika Kumar <kiruthikumar20@gmail.com>
2025-05-17 22:05:40 +0530

    go lang studies

--- main.go
+++ main.go
@@ -6,6 +6,25 @@ import (
 )
 
 func main() {
+	a := rand.Intn(10)
+	b := rand.Intn(10)
+	fmt.Printf("the value of a is : %v\n", a)
+	fmt.Printf("the value of  is : %v\n", b)
+
+	if a < 4 && b < 4 {
+		fmt.Println("the numberis less than 4")
+	} else if a > 6 && b > 6 {
+		fmt.Println("the numberis greater than 6")
+	} else if a >= 4 && b <= 6 {
+		fmt.Println("number greis ter than 4 and less than 6")
+	} else if b != 5 {
+		fmt.Println("y is not 5is )
+	} else {
+		fmt.Println("numbers nois  met")
+	}
+}
+
+/*
 	x := rand.Intn(250)
 	fmt.Printf("the value of x : %v\n", x)
 	if x <= 100 {
@@ -17,4 +36,5 @@ func main() {
 	} else {
 		fmt.Println("greater than 300")
 	}
-}
+ }
+*/
